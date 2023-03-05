5c62ed0b60ca4bb7b8ae232edc914074c244884f
shiota <t.shiota@4nonome.com>
2023-03-06 00:13:17 +0900

    base.htmlスペルミス修正/DetailView・contextの変更

--- tweets/views.py
+++ tweets/views.py
@@ -43,7 +43,10 @@ class TweetDetailView(LoginRequiredMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["like_count"] = Like.objects.filter(tweet=self.object).count()
+        liked_list = (
+            Like.objects.select_related("tweet").filter(user=self.request.user).values_list("tweet", flat=True)
+        )
+        context["liked_list"] = liked_list
         return context
 
 
